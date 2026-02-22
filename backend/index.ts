5e2a70ccbb298588bcd1d0d1540c73d8f8c000bf
sumit poudel <sumitpoudel@proton.me>
2026-02-22 13:09:36 +0545

    basic chats

--- backend/index.ts
+++ backend/index.ts
@@ -7,16 +7,25 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "*",
   },
 });
+
+const message: string[] = [];
 
 io.on("connection", (socket) => {
   socket.on("texts", (data) => {
     console.log(data);
+    message.push(data);
+
+    // 🔥 BROADCAST UPDATED ARRAY
+    io.emit("messages", message);
   });
+
+  // Send existing messages on connection
+  socket.emit("messages", message);
 });
 
-httpServer.listen(port, () => {
+httpServer.listen(port, "0.0.0.0", () => {
   console.log("Server started at port ", port);
 });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,9 +3,13 @@ import { socket } from "./components/socket";
 
 const App = () => {
   const [text, setText] = useState("");
+  const [messages, setMessages] = useState([]);
   useEffect(() => {
     socket.on("connect", () => {
       console.log("connected");
+    });
+    socket.on("messages", (data: []): void => {
+      setMessages(data);
     });
   }, []);
   const send = () => {
@@ -15,18 +19,33 @@ const App = () => {
 
   return (
     <>
-      <section className="w-full flex flex-col h-screen bg-[#eee]">
-        <div className="flex-1 text-center align-middle">messages</div>
-        <div className="bg-white h-20 ">
-          <div className="max-w-300 flex justify-center items-center">
+      <section className="flex flex-col h-screen bg-[#eee]">
+        <div className="flex-1 max-w-300 mx-auto p-4 flex flex-col w-full overflow-y-scroll no-scrollbar wrap-anywhere gap-1">
+          {messages.map((msg, index) => (
+            <div
+              className="rounded-2xl text-white bg-blue-500 w-fit px-4 p-1"
+              key={index}
+            >
+              {msg}
+            </div>
+          ))}
+        </div>
+        <div className="bg-white h-15 ">
+          <div className="max-w-300 mx-auto h-full flex gap-1 justify-center items-center">
             <input
-              className="border-red-500 border-2"
+              className="border-[#aeaeae] rounded-l-full flex-1 pl-4 rounded-r-full focus:outline-none h-9 border-2"
               value={text}
               onChange={(e) => {
                 setText(e.target.value);
               }}
+              placeholder="Message"
             />
-            <button onClick={send}>Send</button>
+            <button
+              className="bg-blue-500 rounded-l-full w-20 rounded-r-full text-white h-9 px-4"
+              onClick={send}
+            >
+              send
+            </button>
           </div>
         </div>
       </section>
